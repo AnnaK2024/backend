ecae5e0c1f2659454deea8eeb196978564e12c4d
annak2024 <kalininaann.pk@mail.ru>
2025-10-05 21:05:21 +0300

    Error

--- src/app.js
+++ src/app.js
@@ -14,8 +14,10 @@ const {
   MONGO_URL = "mongodb://127.0.0.1:27017/backend",
 } = process.env;
 
-mongoose.connect(MONGO_URL);
-console.log("Connected to MongoDb");
+mongoose
+  .connect(MONGO_URL)
+  .then(() => console.log("Connected to MongoDb"))
+  .catch((error) => console.log(error));
 
 const app = express();
 
