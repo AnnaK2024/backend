2bbd868b4ec2c43fb115abd99b1acd91535f502e
annak2024 <kalininaann.pk@mail.ru>
2025-10-06 23:04:13 +0300

    не подключается Монго

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,5 +1,5 @@
-const cors = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1');
+const cors = (request, response, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, HEAD, POST, PATCH, DELETE');
     next();
--- src/routes/books.js
+++ src/routes/books.js
@@ -4,16 +4,16 @@ const loggerBooks = require("../middlewares/loggerBooks");
 const {
   getBooks,
   getBook,
-  updateBook,
+  updateStatusBook,
   createBook,
   deleteBook,
 } = require("../controllers/books");
 
-router.use(loggerBooks)
+router.use(loggerBooks);
 
 router.get("/books", getBooks); //Список книг
 router.get("/books/:book_id", getBook); //Получить определенную книгу по id
-router.patch("/books/:book_id", updateBook); //Обновление статуса книги
+router.patch("/books/:book_id", updateStatusBook); //Обновление статуса книги
 router.post("/books", createBook); //Добавление книги
 router.delete("/books/:book_id", deleteBook); //Удаление книги
 
